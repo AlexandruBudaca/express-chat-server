10d3a5edbe3df4c9f5bf23b5cc6867bfd8cd0362
Alex <alex.andrul10@gmail.com>
2020-06-30 19:43:21 +0100

    new id

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.post("/messages", (req, res) => {
   req.body.from === "" || req.body.text === ""
     ? res.send("Sorry! Please complete all the fields.").sendStatus(400)
     : messages.push({
-        id: messages.map((id) => {
+        id: messages.find((id) => {
           messages.id === id.id ? messages.length - 1 + 1 : messages.length + 1;
         }),
 
